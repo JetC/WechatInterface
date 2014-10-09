71411b98a1b48ab97e8ecd521602417cf3dc781b
Sun PeiFeng <g7733pf@gmail.com>
2014-10-09 20:49:57 +0800

    Initial Commit

---
+++ WechatInterface/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WechatInterface
+//
+//  Created by 孙培峰 on 10/9/14.
+//  Copyright (c) 2014 孙培峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WechatInterface/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  WechatInterface
+//
+//  Created by 孙培峰 on 10/9/14.
+//  Copyright (c) 2014 孙培峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
